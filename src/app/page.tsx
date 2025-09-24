92a4887eead7c29e9b2bc3a8d670f739c17ba2d4
diegorandal@netbook <diegorandal@gmail.com>
2025-09-24 09:06:15 -0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
-import { Page } from '@/components/PageLayout';
-import { AuthButton } from '../components/AuthButton';
+'use client';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { walletAuth } from '@/auth/wallet';
 
 export default function Home() {
-  return (
-    <Page>
-      <Page.Main className="flex flex-col items-center justify-center">
-        <AuthButton />
-      </Page.Main>
-    </Page>
-  );
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === 'loading') {
+    return (
+      <main className="flex items-center justify-center min-h-screen">
+        <p>Cargando...</p>
+      </main>
+    );
+  }
+
+  if (status === 'authenticated') {
+    router.push('/home');
+    return null; // No renderizar nada mientras se redirige
+  }
+
+  if (status === 'unauthenticated') {
+    walletAuth();
+    return (
+      <main className="flex items-center justify-center min-h-screen">
+        <p>Iniciando autenticación con la billetera...</p>
+      </main>
+    );
+  }
+
+  // Fallback, en caso de un estado inesperado
+  return null;
 }
